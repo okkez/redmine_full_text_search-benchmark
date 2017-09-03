aa724d4917a0afa5a1c9d9dedcaad5249c6ff171
okkez <okkez000@gmail.com>
2017-09-03 15:56:04 +0900

    Ignore fluentd

--- generate-hosts.rb
+++ generate-hosts.rb
@@ -7,6 +7,7 @@ ip = JSON.parse(`terraform output -json ip`)
 File.open("ansible/hosts", "w+") do |file|
   file.puts("[servers]")
   ip["value"].each do |name, (public, private)|
+    next unless /redmine/.match?(name)
     file.puts("#{name} ansible_host=#{public} public_ip=#{public} private_ip=#{private}")
   end
 end
